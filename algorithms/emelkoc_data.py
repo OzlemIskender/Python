7b360783b44ec548aa7c04ffc430263c4512ada3
Özlem İskender <58220997+OzlemIskender@users.noreply.github.com>
2021-04-12 21:10:24 +0300

    Add files via upload

---
+++ algorithms/emelkoc_data.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 10 15:44:48 2021
+
+@author: OZLEM
+"""
+import nilearn
+from nilearn import plotting
+plotting.plot_glass_brain("C:/Users/OZLEM/Desktop/Data_Ozlem/UM_1_0050366_reho.nii")   
